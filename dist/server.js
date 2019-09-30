85408ab5fa060bbb77526ee7c60ae77e367374ad
koskh <koskh@yandex.ru>
2019-09-30 09:54:04 +0200

    #master server.use(express.static('/'));

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 
 const server = express();
-server.use(express.static('dist'));
+server.use(express.static('/'));
 
 server.listen(port, () => {
     console.log(`App listening on port ${port}`);
